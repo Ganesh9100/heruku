804f37709d2b0f15ccde6f7b2798775489fbce9b
Ganesh Raj <60533730+Ganesh9100@users.noreply.github.com>
2020-11-24 11:52:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Predicted Employee Salary {}'.format(output))
 
 
 if __name__ == "__main__":
